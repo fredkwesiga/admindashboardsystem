b783da93790aa31f6c9555266c57d90bc98f33cd
fred kwesiga <fredkwesiga9@gmail.com>
2022-06-27 17:18:13 +0300

    creates form for add new user in New.jsx

--- src/formSource.js
+++ src/formSource.js
@@ -3,25 +3,25 @@ export const userInputs = [
     id: 1,
     label: 'Username',
     type: 'text',
-    placeholder: 'john_doe',
+    placeholder: 'komugisha_jane',
   },
   {
     id: 2,
     label: 'Name and surname',
     type: 'text',
-    placeholder: 'John Doe',
+    placeholder: 'Jane Komugisha',
   },
   {
     id: 3,
     label: 'Email',
     type: 'mail',
-    placeholder: 'john_doe@gmail.com',
+    placeholder: 'komugisha@gmail.com',
   },
   {
     id: 4,
     label: 'Phone',
     type: 'text',
-    placeholder: '+1 234 567 89',
+    placeholder: '+256 79818181',
   },
   {
     id: 5,
@@ -32,13 +32,13 @@ export const userInputs = [
     id: 6,
     label: 'Address',
     type: 'text',
-    placeholder: 'Elton St. 216 NewYork',
+    placeholder: 'Muyega Tankhill',
   },
   {
     id: 7,
     label: 'Country',
     type: 'text',
-    placeholder: 'USA',
+    placeholder: 'Uganda',
   },
 ];
 
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,8 +1,12 @@
 import './new.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
+import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
+import { useState } from 'react';
 
-const New = ({title}) => {
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState('');
+
   return (
     <div className="new">
       <Sidebar />
@@ -14,36 +18,34 @@ const New = ({title}) => {
         <div className="bottom">
           <div className="left">
             <img
-              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+              }
               alt=""
             />
           </div>
           <div className="right">
             <form>
               <div className="formInput">
-                <label htmlFor="name">Name</label>
-                <input type="text" placeholder="komugisha" />
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: 'none' }}
+                />
               </div>
-              <div className="formInput">
-                <label htmlFor="email">Email </label>
-                <input type="email" placeholder="komugisha@gmail.com" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="email">Email </label>
-                <input type="text" placeholder="+256 78929929 " />
-              </div>
-              <div className="formInput">
-                <label htmlFor="email">Password </label>
-                <input type="password" placeholder="" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="email">Address </label>
-                <input type="password" placeholder="Muyega" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="email">Address </label>
-                <input type="password" placeholder="Muyega" />
-              </div>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
               <button>Send</button>
             </form>
           </div>
@@ -51,6 +53,6 @@ const New = ({title}) => {
       </div>
     </div>
   );
-}
+};
 
-export default New
+export default New;
